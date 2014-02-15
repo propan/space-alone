4d28f2856aed60d031cc5daeafc9d7752a6b6716
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2014-02-15 22:37:24 +0200

    update dependencies versions

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/propan/space-alone"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
